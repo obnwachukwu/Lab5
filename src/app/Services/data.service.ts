a5cdc7f21901bd25229c3b82babccc9407562d45
unknown <g00435305@atu.ie>
2025-03-08 00:13:15 +0000

    Lab5 completed

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -10,11 +10,13 @@ export class DataService { // DataService class
   //constructor to build the htttpClient request
   constructor(private httpClient:HttpClient) { }
 
+    //Getting data from student from API request
   getStudentData(): Observable<any>{
     // Making a GET request to the student data API and returning the response as an observable
     return this.httpClient.get('https://jsonblob.com/api/jsonblob/1336347674273832960');
   }
 
+  //Getting weather data from API
   getWeatherData(): Observable<any>{
     //returning the requested data from the WeatherAPI
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
