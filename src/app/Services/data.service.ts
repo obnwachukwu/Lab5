c6b79a8f91e8157a083b852d78248e2839cca542
unknown <g00435305@atu.ie>
2025-03-08 00:08:01 +0000

    Lab5 completed

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -5,15 +5,18 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService { // DataService class
 
+  //constructor to build the htttpClient request
   constructor(private httpClient:HttpClient) { }
 
   getStudentData(): Observable<any>{
+    // Making a GET request to the student data API and returning the response as an observable
     return this.httpClient.get('https://jsonblob.com/api/jsonblob/1336347674273832960');
   }
 
   getWeatherData(): Observable<any>{
+    //returning the requested data from the WeatherAPI
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
   }
-}
+}//the end of the code
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,25 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule], //an array of the imports 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
 export class AppComponent implements OnInit{
+  //atributes
   students:any[] = [];
   weather:any[] = [];
   temperature:any = "";
+  //setting up the service
   constructor(private dataService: DataService){
 
   }
 
+  //requesting data from API for students
   ngOnInit(): void {
+    //getting students
     this.dataService.getStudentData().subscribe(
       (data)=>{
         this.students = data.students;
@@ -29,8 +33,9 @@ export class AppComponent implements OnInit{
 
     this.dataService.getWeatherData().subscribe(
       (data)=>{
+        //getting weather and temperature
         this.weather = data.weather;
-        this.temperature = (data.main.temp - 273.155).toFixed(2);
+        this.temperature = (data.main.temp - 273.155).toFixed(2); //toFixed function to round temperature to 2 decimal places
       }
     )
   }
